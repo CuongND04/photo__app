5af5f702f8aa0fa6a11a40e468ae633bae18aa84
Nguyễn Đức Cường <uteu237@gmail.com>
2025-10-29 17:00:08 +0700

    commit_main_2

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -17,6 +17,16 @@ const isAuthenticated = async (req, res, next) => {
   }
   req.user = user;
   next();
+
+  // const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  // const user = await User.findById(decoded.id);
+  // if (!user) {
+  //   return res.status(404).json({
+  //     message: "User not found",
+  //   });
+  // }
+  // req.user = user;
+  // next();
 };
 
 module.exports = isAuthenticated;
